43580a0b3dc7e59c8dcbdbf96eaa437bcb39786f
Alberto Benatti <albi.benni8@gmail.com>
2024-06-22 21:05:34 +0200

    fix: use URL object

--- crawl.js
+++ crawl.js
@@ -1,7 +1,7 @@
 export const normalizeURL = (url) => {
-    let endUrl = url.replace("https://", "");
-    endUrl = endUrl.replace("http://", "");
-    
+    const urlObj = new URL(url);
+    let endUrl = `${urlObj.host}${urlObj.pathname}`;
+   
     if (endUrl[endUrl.length-1] === "/") {
         endUrl = endUrl.slice(0, -1);
     }
