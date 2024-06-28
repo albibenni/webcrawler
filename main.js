be1d5d725e091e67fa18d2dd42e12e1cbb4a6039
Alberto Benatti <albi.benni8@gmail.com>
2024-06-29 01:07:26 +0200

    argv

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-const normalizeURL = (url) =>{
-    
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log('no website provided')
+        return
+    }
+    if (process.argv.length > 3) {
+        console.log('too many arguments provided')
+        return
+    }
+    const baseURL = process.argv[2]
+
+    console.log(`starting crawl of: ${baseURL}...`)
 }
+
+main();
